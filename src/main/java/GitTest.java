9074ce3d08b615f5505f99e389e2300946b44311
ss <s13613993739@163.com>
2021-11-29 19:34:55 +0800

    first commit

---
+++ src/main/java/GitTest.java
@@ -0,0 +1,13 @@
+public class GitTest {
+
+    public static void main(String[] args) {
+
+        System.out.println();
+
+        System.out.println("           sss ");
+        System.out.println("           sss ");
+        System.out.println("           sss ");
+
+    }
+
+}
